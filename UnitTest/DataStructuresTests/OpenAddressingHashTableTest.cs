bbe3d1669264a1fea814651a259c1746914863fe
gyupetosz <47506687+gyupetosz@users.noreply.github.com>
2021-05-03 11:24:53 +0200

    I published the tests on the wrong branch
    Deleting the wrong lines

--- UnitTest/DataStructuresTests/OpenAddressingHashTableTest.cs
+++ UnitTest/DataStructuresTests/OpenAddressingHashTableTest.cs
@@ -6,24 +6,5 @@ using Xunit;
 
 namespace UnitTest.DataStructuresTests
 {
-    public static class OpenAddressingHashTableTest
-    {
-        [Fact]
-        public static void AddValueToHashTableTest()
-        {
-            OpenAddressingHashTable<int,string> hashTable = new OpenAddressingHashTable<int, string>(10);
-            hashTable.Add(1, "főzelék");
-            int length = hashTable.Count;
-            Assert.Equal(1,length);
-        }
-        [Fact]
-        public static void AddValueToHashTableTest()
-        {
-            OpenAddressingHashTable<int, string> hashTable = new OpenAddressingHashTable<int, string>(10);
-            hashTable.Add(1, "főzelék");
-            int result = hashTable.search(1);
-            
-            Assert.Equal(1, length);
-        }
-    }
+
 }
