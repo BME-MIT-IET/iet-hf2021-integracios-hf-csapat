a2b308fbc6c20343d2613d91edfbd497a760b503
Akos <semmi>
2021-05-11 15:03:47 +0200

    Benchmark sorting algortihms

--- PerformanceTest/Program.cs
+++ PerformanceTest/Program.cs
@@ -1,15 +1,34 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Diagnostics;
+using Algorithms.Sorting; 
 
 namespace PerformanceTest
 {
     class Program
     {
+        static List<int> list = new List<int>();
         static void Main(string[] args)
         {
+            Random rnd = new Random();
+            for (int i = 0; i < 3000000; i++)
+                list.Add(rnd.Next());
+
+            TestAlgorithm("heap", list => list.HeapSort());
+            TestAlgorithm("merge", list => list.MergeSort());
+            TestAlgorithm("quick", list => list.QuickSort());
+            TestAlgorithm("sort", list => list.Sort());
+
+            Console.ReadKey();
+        }
+
+        public static void TestAlgorithm(string name, Action<List<int>> sortFunction)
+        {
+            List<int> clonedList = new List<int>(list);
+            Stopwatch stopwatch = Stopwatch.StartNew();
+            sortFunction(clonedList);
+            stopwatch.Stop();
+            Console.WriteLine(name + " time: " + stopwatch.Elapsed);
         }
     }
 }
