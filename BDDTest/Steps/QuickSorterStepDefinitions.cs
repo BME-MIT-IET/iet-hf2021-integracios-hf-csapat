46cd776d4181b177234a1a27cd64e6b50534f332
Marcell Harmaci <harmaci.marci@gmail.com>
2021-05-14 21:13:06 +0200

    Change BDD test param names according to Specflow convention

--- BDDTest/Steps/QuickSorterStepDefinitions.cs
+++ BDDTest/Steps/QuickSorterStepDefinitions.cs
@@ -7,14 +7,15 @@ using TechTalk.SpecFlow;
 namespace BDDTest.Steps
 {
 	[Binding]
+	[Scope(Feature = "QuickSorter")]
 	public sealed class QuickSorterStepDefinitions
 	{
 
 		// For additional details on SpecFlow step definitions see https://go.specflow.org/doc-stepdef
 
 		private readonly ScenarioContext _scenarioContext;
-		private List<int> list = new List<int>();
-		private Random random;
+		private List<int> _list = new List<int>();
+		private Random _random;
 
 		public QuickSorterStepDefinitions(ScenarioContext scenarioContext)
 		{
@@ -24,28 +25,28 @@ namespace BDDTest.Steps
 		[Given("the random seed is (.*)")]
 		public void GivenTheRandomSeedIs(int randomSeed)
 		{
-			random = new Random(randomSeed);
+			_random = new Random(randomSeed);
 		}
 
-		[Given("the list is of length (.*)")]
+		[Given("a random list of length (.*) is generated")]
 		public void GivenListIsOfLength(int length)
 		{
 			for (int i = 0; i < length; i++)
 			{
-				list.Add(random.Next());
+				_list.Add(_random.Next());
 			}
 		}
 
 		[When("the list is sorted")]
 		public void WhenTheListIsSorted()
 		{
-			list.QuickSort<int>();
+			_list.QuickSort<int>();
 		}
 
 		[Then("the list should be in ascending order")]
 		public void ThenTheListShouldBeAscending()
 		{
-			list.Should().BeInAscendingOrder(x => x);
+			_list.Should().BeInAscendingOrder(x => x);
 		}
 	}
 }
