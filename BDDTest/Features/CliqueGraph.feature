bc744ff23acfd9505f18f94044ec3544ac2655d8
Marcell Harmaci <harmaci.marci@gmail.com>
2021-05-15 00:24:44 +0200

    Add some CliqueGraph bdd tests

---
+++ BDDTest/Features/CliqueGraph.feature
@@ -0,0 +1,19 @@
+﻿Feature: CliqueGraph
+	Represents an unweighted undirected graph, modeling with a set of its maximal complete subgraphs of it.
+	Should be fast in clustered graphs
+
+@addVertices
+Scenario: Add vertices to the graph as a list
+	Given the graph is empty
+	When these vertices are added |ab, bc, cd, de, ef|
+	Then the graph's vertices should be |ab, bc, cd, de, ef|
+
+@addVertices
+Scenario: Add vertices to the graph one-by-one
+	Given the graph is empty
+	When this vertex is added ab
+	And this vertex is added bc
+	And this vertex is added cd
+	And this vertex is added de
+	And this vertex is added ef
+	Then the graph's vertices should be |ab, bc, cd, de, ef|
---
+++ BDDTest/Steps/CliqueGraphStepDefinitions.cs
@@ -0,0 +1,50 @@
+﻿using DataStructures.Graphs;
+using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TechTalk.SpecFlow;
+
+namespace BDDTest.Steps
+{
+	[Binding]
+	[Scope(Feature = "CliqueGraph")]
+	public sealed class CliqueGraphStepDefinitions
+	{
+		// For additional details on SpecFlow step definitions see https://go.specflow.org/doc-stepdef
+
+		private readonly ScenarioContext _scenarioContext;
+		private CliqueGraph<string> _cliqueGraph;
+		private List<string> _vertices = new List<string>();
+
+		public CliqueGraphStepDefinitions(ScenarioContext scenarioContext)
+		{
+			_scenarioContext = scenarioContext;
+		}
+
+		[Given("the graph is empty")]
+		public void GivenTheGraphIsEmpty()
+		{
+			_cliqueGraph = new CliqueGraph<string>();
+		}
+
+		[When("these vertices are added (.*)")]
+		public void WhenTheseVerticesAreAdded(List<string> vertices)
+		{
+			_cliqueGraph.AddVertices(vertices);
+		}
+
+		[When("this vertex is added (.*)")]
+		public void WhenTheseVerticesAreAdded(string vertex)
+		{
+			_cliqueGraph.AddVertex(vertex);
+		}
+
+		[Then("the graph's vertices should be (.*)")]
+		public void ThenTheGraphsVerticesShouldBe(List<string> expectedVertices)
+		{
+			_cliqueGraph.Vertices.Should().BeEquivalentTo(expectedVertices);
+		}
+	}
+}
