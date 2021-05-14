04d16a56953a1ee226ffc960edf90bcdc679d813
Marcell Harmaci <harmaci.marci@gmail.com>
2021-05-15 01:50:24 +0200

    Add CycleSorter bdd tests

---
+++ BDDTest/Steps/CycleSorterStepDefinitions.cs
@@ -0,0 +1,71 @@
+﻿using Algorithms.Sorting;
+using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TechTalk.SpecFlow;
+
+namespace BDDTest.Steps
+{
+	[Binding]
+	[Scope(Feature = "CycleSorter")]
+	public sealed class CycleSorterStepDefinitions
+	{
+		private readonly ScenarioContext _scenarioContext;
+		private List<int> _list = new List<int>();
+		private Random _random;
+
+		public CycleSorterStepDefinitions(ScenarioContext scenarioContext)
+		{
+			_scenarioContext = scenarioContext;
+		}
+
+		[Given("the random seed is (.*)")]
+		public void GivenTheRandomSeedIs(int randomSeed)
+		{
+			_random = new Random(randomSeed);
+		}
+
+		[Given("a random list of length (.*) is generated")]
+		public void GivenListIsOfLength(int length)
+		{
+			_list.Clear();
+
+			for (int i = 0; i < length; i++)
+			{
+				_list.Add(_random.Next());
+			}
+		}
+
+		[When("the list is sorted")]
+		public void WhenTheListIsSorted()
+		{
+			_list.CycleSort<int>();
+		}
+
+		[When("the list is sorted ascending")]
+		public void WhenTheListIsSortedAscending()
+		{
+			_list.CycleSortAscending<int>(Comparer<int>.Default);
+		}
+
+		[When("the list is sorted descending")]
+		public void WhenTheListIsSortedDescending()
+		{
+			_list.CycleSortDescending<int>(Comparer<int>.Default);
+		}
+
+		[Then("the list should be in ascending order")]
+		public void ThenTheListShouldBeAscending()
+		{
+			_list.Should().BeInAscendingOrder(x => x);
+		}
+
+		[Then("the list should be in descending order")]
+		public void ThenTheListShouldBeDescending()
+		{
+			_list.Should().BeInDescendingOrder(x => x);
+		}
+	}
+}
